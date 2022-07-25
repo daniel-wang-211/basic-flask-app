7f431b7a59feb91a12801974624ea6cb706317d8
daniel-wang-211 <daniel.wang@verkada.com>
2022-07-25 11:18:19 -0700

    Add comment.

--- app/api1/api.py
+++ app/api1/api.py
@@ -5,6 +5,7 @@ api1 = Blueprint("api1", __name__, template_folder="templates")
 
 @api1.route("/hello", methods=["GET"])
 def sub_api1() -> Response :
+    """A test api"""
     name = "Jane"
     args = request.args
     if "name" in args:
